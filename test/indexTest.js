5244a2e1b607a9003f28dff66ee48e82ad113609
Lisa Lee <snickers495@gmail.com>
2018-01-15 17:05:31 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,18 +1,6 @@
 const expect = chai.expect;
 
 describe('closures', function() {
-  describe('createDriver', function(){
-    const Driver = createDriver()
-
-    it('returns a class that allows us to create a driver with a name attribute', function(){
-      expect(new Driver('Sam').name).to.equal('Sam')
-    })
-
-    it('increments the driverId each time a driver is created', function(){
-      expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
-    })
-  })
-  
   describe('produceDrivingRange', function(){
     it('returns a function', function(){
       expect(typeof produceDrivingRange()).to.equal('function')
@@ -40,6 +28,18 @@ describe('closures', function() {
       let tenPercentTip = produceTipCalculator(.10)
       expect(tenPercentTip(50)).to.equal(5)
       expect(tenPercentTip(30)).to.equal(3)
+    })
+  })
+
+  describe('createDriver', function(){
+    const Driver = createDriver()
+
+    it('returns a class that allows us to create a driver with a name attribute', function(){
+      expect(new Driver('Sam').name).to.equal('Sam')
+    })
+
+    it('increments the driverId each time a driver is created', function(){
+      expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
     })
   })
 })
