b9cbe04e36448493e5003b234b547d492c33a934
Lisa Lee <snickers495@gmail.com>
2018-01-15 17:05:03 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,18 @@
 const expect = chai.expect;
 
 describe('closures', function() {
+  describe('createDriver', function(){
+    const Driver = createDriver()
+
+    it('returns a class that allows us to create a driver with a name attribute', function(){
+      expect(new Driver('Sam').name).to.equal('Sam')
+    })
+
+    it('increments the driverId each time a driver is created', function(){
+      expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
+    })
+  })
+  
   describe('produceDrivingRange', function(){
     it('returns a function', function(){
       expect(typeof produceDrivingRange()).to.equal('function')
