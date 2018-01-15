92ad85accd8fbbb00f1f1696468861dfe064c9c9
Lisa Lee <snickers495@gmail.com>
2018-01-15 16:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
  function produceDrivingRange(range) {
-   if (range < 10)
+   return function(block1, block2) {
+     const rangeVal = Math.abs(parseInt(block1.slice(0, 1)) - parseInt(block2.slice(0, 1)))
+     if (rangeVal < range) {
+       return `within range by ${(range - rangeVal)}`
+     }
+     else {
+       return `${rangeVal - range} blocks out of range`
+     }
+   }
  }
