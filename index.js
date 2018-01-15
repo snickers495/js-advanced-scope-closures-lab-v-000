0f2b4fb641669da18beaa1fc5e677bd23c6bc2fb
Lisa Lee <snickers495@gmail.com>
2018-01-15 17:07:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+ function produceDrivingRange(range) {
+   return function(block1, block2) {
+     const rangeVal = Math.abs(parseInt(block1.slice(0, 2)) - parseInt(block2.slice(0, 2)))
+     if (rangeVal < range) {
+       return `within range by ${(range - rangeVal)}`
+     }
+     else {
+       return `${rangeVal - range} blocks out of range`
+     }
+   }
+ }
+function produceTipCalculator(percent){
+    return function(total) {
+      return total * percent;
+    }
+}
+function createDriver(){
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
