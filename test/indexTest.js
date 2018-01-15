02b1ec42d467782368b47bc2fa2d3c2920368dad
Lisa Lee <snickers495@gmail.com>
2018-01-15 17:04:52 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -30,16 +30,4 @@ describe('closures', function() {
       expect(tenPercentTip(30)).to.equal(3)
     })
   })
-
-  describe('createDriver', function(){
-    const Driver = createDriver()
-
-    it('returns a class that allows us to create a driver with a name attribute', function(){
-      expect(new Driver('Sam').name).to.equal('Sam')
-    })
-
-    it('increments the driverId each time a driver is created', function(){
-      expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
-    })
-  })
 })
